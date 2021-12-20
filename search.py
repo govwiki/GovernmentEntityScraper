92bb4a58e1738c72b3679fc88f2b29690c5f05cb
Christopher Gioia <christophergioia64@gmail.com>
2021-12-19 20:32:32 -0800

    end-to-end google search of texas entity urls

--- search.py
+++ search.py
@@ -2,91 +2,161 @@ import urllib
 import requests
 from bs4 import BeautifulSoup
 import re
+import url_checker
+import logging
+import datetime
+import sys
+import time
 
-class Search:
-    def __init__(self):
-        pass
-    def scrapeResults(self, query):
-        # desktop user-agent
-        USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
-        # mobile user-agent
-        MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+import openpyxl
+from pathlib import Path
+import concurrent.futures
 
-        query = query.replace(' ', '+')
-        URL = f"https://google.com/search?q={query}"
+# Setup logging
+now = datetime.datetime.now()
+date_time = now.strftime("%Y-%m-%d %H-%M-%S")
 
-        headers = {"user-agent": USER_AGENT}
-        resp = requests.get(URL, headers=headers)
+logger = logging.getLogger()
+handler = logging.FileHandler(
+    mode="w", filename="logs/search_" + date_time + ".txt")
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
-        if resp.status_code == 200:
-            soup = BeautifulSoup(resp.content, "html.parser")
-            results = []
-            for g in soup.find_all('div', class_='r'):
-                anchors = g.find_all('a')
-                if anchors:
-                    link = anchors[0]['href']
-                    title = g.find('h3').text
-                    item = {
-                        "title": title,
-                        "link": link
-                    }
-                    results.append(item)
-            return results
-
-    def getLeftover(self, url):
-        indexOfLastPeriod = url.rfind('.')
-        domainName = re.findall('^([\w\-]+)', url[indexOfLastPeriod+1:])
-        domainName = domainName[0]
-        leftover = re.sub(domainName,"",url[indexOfLastPeriod+1:],count=1)
-        return leftover.lower()
-
-    def removeHomeFromLeftover(self, leftover):
-        if 'home' in leftover:
-            if '/' == leftover[0]:
-                leftover = re.sub("/","", leftover, count = 1)
-            leftover = re.sub("home","",leftover,count=1)
-        return leftover
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 
-    def findUrl(self, results):
-        urlDict = {}
-        resultsSeen = 0
-        for result in results:
-            if 'wikipedia' in result['link']:
-                urlDict['exists'] = False
-                return urlDict
-            else:
-                leftover = self.getLeftover(result['link'])
-                leftover = self.removeHomeFromLeftover(leftover)
-                if leftover == '' or leftover == '/':
-                    urlDict['exists'] = True
-                    urlDict['link'] = result['link']
-                    return urlDict
-                else:
-                    pass
-            resultsSeen += 1
-            if resultsSeen == 4:
-                urlDict['exists'] == False
-                return urlDict      
+def getBingSearchResults(query):
+    # desktop user-agent
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+
+    query = query.replace(' ', '+')
+    URL = f"https://bing.com/search?q={query}"
+
+    headers = {"user-agent": USER_AGENT}
+    resp = requests.get(URL, headers=headers)
+
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.content, "html.parser")
+        results = []
+        for g in soup.find_all('ol', id='b_results'):
+            anchors = g.find_all('a')
+            for anchor in anchors:
+                if 'href' in anchor.attrs:
+                    href = anchor['href']
+                    if '/search' not in href and 'http' in href:
+                        results.append(href)
+                        # print(str(href))
+        return results
+    else:
+        print("invalid code")
+        return []
 
 
+def getGoogleSearchResults(query):
+    # desktop user-agent
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+
+    query = query.replace(' ', '+')
+    URL = f"https://google.com/search?q={query}"
+
+    headers = {"user-agent": USER_AGENT}
+    resp = requests.get(URL, headers=headers)
+
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.content, "html.parser")
+        results = []
+        for g in soup.find_all('div', 'tF2Cxc'):
+            anchors = g.find_all('a')
+            for anchor in anchors:
+                if 'href' in anchor.attrs:
+                    href = anchor['href']
+                    if '/search' not in href and 'http' in href:
+                        results.append(href)
+                        # print(str(href))
+        return results
+    else:
+        print("invalid code")
+        return []
 
 
-# print(removeHomeFromLeftover(getLeftover('https://www.hamptoncountysc.org/439/Varnville')))
-# print(removeHomeFromLeftover(getLeftover('http://www.varnvillesc.org/page/government')))
-
-# query =  "town of varnville sc"
-# results = scrapeResults(query)
-
-# urlDict = findUrl(results)      ''' urlDict has 2 keys:
-#                                 'exists' which is either 0 if url exists otherwise -1;
-#                                 'link' which containts the url of the website'''
-
-# if urlDict['exists'] == True:
-#     print(urlDict['link'])
-
-# for result in results:
-#     print(result['title'], ":\n", result['link'])
+def getMatchingLink(query):
+    links = getGoogleSearchResults(query)
+    matchingLink = ""
+    for link in links:
+        if 'wikipedia' not in link and 'facebook' not in link:
+            status = url_checker.getStatusCode(link)
+            if status == 200:
+                matchingLink = link
+                break
+    return matchingLink
 
 
+def iterate(excel_filename, tab_name, column, output_file=None, fn=None,
+            suffix="",
+            header_exists=True, debug=False,
+            startRow=1, parallel=True):
+    xlsx_file = Path(excel_filename)
+    wb_obj = openpyxl.load_workbook(xlsx_file)
+    wsheet = wb_obj[tab_name]
+    rowNumber = 1
+    if header_exists:
+        rowNumber = 2
+    statusCodeMap = {}
+    collection = []
+    book = openpyxl.load_workbook(output_file)
+    increment = 10
+    start = time.time()
+    if parallel:
+        while rowNumber < startRow:
+            rowNumber += 1
 
+        while rowNumber < wsheet.max_row:
+            entities = getEntities(
+                rowNumber, wsheet, increment, column, suffix)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                for index, (entityName, entityUrl) in enumerate(zip(entities, executor.map(fn, entities))):
+                    book['Sheet1']['A' +
+                                   str(index+rowNumber)].value = entityName
+                    book['Sheet1']['B' +
+                                   str(index+rowNumber)].value = entityUrl
+                    # print("%s -- %s" % (entityName, entityUrl))
+            book.save(output_file)
+            elapsed = time.time() - start
+            print("%d  (%.1f)" % (rowNumber, elapsed))
+            rowNumber += increment
+    else:
+        for row in wsheet.iter_rows(max_row=wsheet.max_row):
+            rowNumber += 1
+            if rowNumber < startRow:
+                continue
+            nameCell = column + str(rowNumber)
+            entityName = wsheet[nameCell].value + " " + suffix
+
+            res = fn(entityName)
+            book['Sheet1']['A' + str(rowNumber)].value = entityName
+            book['Sheet1']['B' + str(rowNumber)].value = res
+            if rowNumber % 10 == 0:
+                elapsed = time.time() - start
+                print("%d (%.1f)" % (rowNumber, elapsed))
+                book.save(output_file)
+
+    book.save(output_file)
+
+
+def getEntities(start_row, wsheet, increment, column, suffix):
+    max_row = min(start_row+increment, wsheet.max_row)
+    entities = []
+    for row in range(start_row, max_row+1, 1):
+        nameCell = column + str(row)
+        entityName = wsheet[nameCell].value + " " + suffix
+        entities.append(entityName)
+    return entities
+
+
+iterate('Texas Local Governments.xlsx', 'Census of Govts',
+        'D', output_file='texas_websites_1.xlsx', fn=getMatchingLink,
+        suffix="Texas",
+        debug=True, parallel=False, startRow=500)
